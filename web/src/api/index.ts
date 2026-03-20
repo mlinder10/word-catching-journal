34f88edbac136f732419ba73796b9a035ae43d41
Matt Linder <108966946+mlinder10@users.noreply.github.com>
2026-03-20 16:18:02 -0400

    made server url an env variable

--- web/src/api/index.ts
+++ web/src/api/index.ts
@@ -4,5 +4,5 @@ import API from "./api";
 
 export * from "./responses";
 
-export default new API("http://localhost:3001/api/v1");
+export default new API(import.meta.env.VITE_SERVER_URL);
 // export default new MockAPI();
